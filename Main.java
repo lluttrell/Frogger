e85b08d93d80e7ce0caceb752e23a5b3b2e860df
Richard Williams <richyw@uvic.ca>
2017-10-16 09:08:51 -0600

    Added comments, cleaned up whitespace, moved the win checker outside of the move if block.

--- Main.java
+++ Main.java
@@ -36,6 +36,7 @@ public class Main {
 		Log log1 = new Log(3, screen.getScreenWidth() - 3, RIVER_STARTING_Y);
 		car1.initCar(screen, car1);
 		log1.initLog(screen, log1);
+		
 		// Input from player
 		Scanner scanner = new Scanner(System.in);
 		char input;
@@ -47,24 +48,28 @@ public class Main {
 		while (running) {
 			screen.PrintScreen();
 			System.out.println("Lives remaining:" + frog.getLives());
+
 			// Get input from player and do something
 			char keyPressed = scanner.nextLine().charAt(0);
 			if (keyPressed == 'w') {
 				frog.moveUp(screen, frog);
-				if (frog.getY() < 1) {
-					//win state
-					running = false;
-					System.out.println("\n**YOU WIN**");
-				}
-			}else if (keyPressed == 's' && frog.getY() < FROG_STARTING_Y) {
+			} else if (keyPressed == 's' && frog.getY() < FROG_STARTING_Y) {
 				frog.moveDown(screen, frog);
-			}else if (keyPressed == 'a' && frog.getX()  > SCREEN_WIDTH - (SCREEN_WIDTH - 1)) {
+			} else if (keyPressed == 'a' && frog.getX()  > SCREEN_WIDTH - (SCREEN_WIDTH - 1)) {
 				frog.moveLeft(screen, frog);
 			}else if (keyPressed == 'd' && frog.getX() < SCREEN_WIDTH - 2) {
 				frog.moveRight(screen, frog);
 			}else {
 				System.out.println("\nPlease use wasd or stay within game walls.");
 			}
+
+			// check if frog has made to to the finish area
+			if (frog.getY() < 1) {
+				running = false;
+				System.out.println("\n**YOU WIN**");
+			}
+
+			// check if frog overlaps with car. Kill frog if it does.
 			if (car1.overlapsWith(frog)) {
 				frog.die();
 				frog.setX(FROG_STARTING_X);
@@ -75,6 +80,9 @@ public class Main {
 					System.out.println("\n**GAME OVER**");
 				}
 			}
+
+			// check if frog is in the river and if it overlaps with log. Kill
+			// frog if it doesn't
 			if (frog.getY() == RIVER_STARTING_Y && !log1.overlapsWith(frog)) {
 				frog.die();
 				screen.ClearScreenLocation(frog.getX(),frog.getY());
@@ -87,12 +95,14 @@ public class Main {
 				}
 			}
 
+			// reset car to right side of screen if it hits the wall
 			if (car1.getX() > SCREEN_WIDTH - (SCREEN_WIDTH - 2)) {
 				car1.moveLeft(screen, car1);
 			} else { //Car hits left wall
 				car1.resetRight(screen, car1, wall);
 			}
 
+			// reset log to right side of screen if it hits the wall
 			if (log1.getX() > SCREEN_WIDTH - (SCREEN_WIDTH - 2)) {
 				log1.moveLeft(screen, log1);
 			} else { //Log hits left wall
