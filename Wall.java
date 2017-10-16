a71c899b8f211d4b22b39d28e1177d22e186e22e
Richard Williams <richyw@uvic.ca>
2017-10-16 09:30:39 -0600

    Added JavaDoc Comments to Wall class

--- Wall.java
+++ Wall.java
@@ -6,22 +6,39 @@
 
 public class Wall extends GameObject {
 
+	/**
+	* Default constructor. Sets symbol to '#'
+	*/
 	public Wall() {
 		setSymbol('#');
 	}
 
+	/**
+	* Constructor for Wall
+	* @param symbol Character used to represent the wall
+	*/
 	public Wall(char symbol) {
 		setSymbol(symbol);
 	}
 
-	// Add horizontal line of walls
+	/**
+	* Adds horizontal line of walls to GameScreen
+	* @param screen GameScreen to draw Wall on
+	* @param wall Wall to draw on GameScreen
+	* @param rowNumber Row on gamescreen to draw wall to
+	*/
 	public void addWallsRow(GameScreen screen, Wall wall, int rowNumber) {
 		for (int i = 0; i < screen.getScreenWidth(); i++) {
 			screen.setObjectOnLocation(wall, i, rowNumber);
 		}
 	}
 
-	// Add vertical line of walls
+	/**
+	* Adds vertical line of wall to GameScreen
+	* @param screen GameScreen to draw Wall on
+	* @param wall Wall to draw on GameScreen
+	* @param columnNumber Column on gamescreen to draw wall to
+	*/
 	public void addWallsColumn(GameScreen screen, Wall wall, int columnNumber) {
 		for (int i = 0; i < screen.getScreenHeight(); i++) {
 			screen.setObjectOnLocation(wall, columnNumber, i);
