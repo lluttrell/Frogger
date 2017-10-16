eabd2c6173ac7aa7e16d6271f94fda42ed4bc2fb
Richard Williams <richyw@uvic.ca>
2017-10-16 09:14:07 -0600

    Added JavaDoc Comments to GameObject

--- GameObject.java
+++ GameObject.java
@@ -10,27 +10,51 @@ public class GameObject {
 	private char symbol;
 
 	// Getters
+	/**
+	* Returns the x coordinate of the GameObject.
+	* @return x Coordinate of the GameObject
+	*/
 	public int getX() {
 		return this.x;
 	}
 
+	/**
+	* Returns the y coordinate of the GameObject.
+	* @return y Coordinate of the GameObject
+	*/
 	public int getY() {
 		return this.y;
 	}
 
+	/**
+	* Returns the character used to represent the GameObject
+	* @return char used to represent the GambeObject
+	*/
 	public char getSymbol() {
 		return symbol;
 	}
 
 	// Setters
+	/**
+	* Sets the x coordinate of the GameObject.
+	* @param newLocation New x coordinate
+	*/
 	public void setX(int newLocation) {
 		this.x = newLocation;
 	}
 
+	/**
+	* Sets the x coordinate of the GameObject.
+	* @param newLocation New y coordinate
+	*/
 	public void setY(int newLocation) {
 		this.y = newLocation;
 	}
 
+	/**
+	* Sets the text character used to draw the GameObject.
+	* @param newSymbol New character
+	*/
 	public void setSymbol(char newSymbol) {
 		this.symbol = newSymbol;
 	}
