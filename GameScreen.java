b07a6509f66812c440c595374f79ee4ed24356b3
Richard Williams <richyw@uvic.ca>
2017-10-16 09:24:30 -0600

    Added javadoc comments to GameScreen

--- GameScreen.java
+++ GameScreen.java
@@ -8,12 +8,20 @@ public class GameScreen {
   private int width, height;
 	private char[][] screenMatrix;
 
+  /**
+  * Default constructor for GameScreen object
+  * @param width Width of GameScreen
+  * @param height Height of GameScreen
+  */
 	public GameScreen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		this.screenMatrix = new char[this.height][this.width];
 	}
 
+  /**
+  * Fills screenMatrix (play area) with default characters
+  */
   public void InitScreen() {
 		for (int i = 0; i < this.height; i++) {
 			for (int j = 0; j < this.width; j++) {
@@ -22,6 +30,9 @@ public class GameScreen {
 		}
 	}
 
+  /**
+  * Prints the screenMatrix to the console
+  */
   public void PrintScreen() {
 		for (int i = 0; i < this.height; i++) {
 			for (int j = 0; j < this.width; j++) {
@@ -31,25 +42,51 @@ public class GameScreen {
 		}
 	}
 
-  //Screen clear location
+  /**
+  * Resets a character in the screenMatrix at a specified location to default.
+  * @param x x coordinate
+  * @param y y coordinate
+  */
 	public void ClearScreenLocation(int x, int y) {
 		this.screenMatrix[y][x] = '*';
 	}
 
 	// Getters
+  /**
+  * Returns the width of the screen
+  * @return width of screen
+  */
 	public int getScreenWidth() {
 		return this.width;
 	}
 
+  /**
+  * Returns the height of the screen
+  * @return height of screen
+  */
 	public int getScreenHeight() {
 		return this.height;
 	}
 
+  /**
+  * Returns the character at a certain position in the screenMatrix
+  * @param x x position in screenMatrix
+  * @param y y position in screenMatrix
+  * @return character at specified location
+  */
 	public char getObjectOnLocation(int x, int y) {
 		return this.screenMatrix[y][x];
 	}
 
 	// Setters
+
+  /**
+  * Updates the character at a specified position in the screenMatrix to the
+  * character used to represent a GameObject
+  * @param object GameObject moving into the position
+  * @param x x coordinate of GameObject
+  * @param y y coordinate of GameObject
+  */
 	public void setObjectOnLocation(GameObject object, int x, int y) {
 		this.screenMatrix[y][x] = object.getSymbol();
 	}
