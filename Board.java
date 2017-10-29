f8d991e772262d6df60a1d63a16625ef64aa7405
idenc <iden.craven@gmail.com>
2017-10-29 13:26:48 -0600

    Made the frog stick to ships

--- Board.java
+++ Board.java
@@ -35,109 +35,118 @@ public class Board extends JPanel implements ActionListener {
 
   public Board() {
 
-      initBoard();
+  	initBoard();
   }
 
   private void initBoard() {
 
-      keyManager = new KeyManager();
+  	keyManager = new KeyManager();
 
-      addKeyListener(keyManager);
-      setFocusable(true);
-      setDoubleBuffered(true);
-      running = true;
+  	addKeyListener(keyManager);
+  	setFocusable(true);
+  	setDoubleBuffered(true);
+  	running = true;
 
-      background = ImageLoader.loadImage("images/background.png");
-      ground = ImageLoader.loadImage("images/metal.png");
+  	background = ImageLoader.loadImage("images/background.png");
+  	ground = ImageLoader.loadImage("images/metal.png");
 
-      frog = new Frog(this, frogXStart, frogYStart);
+  	frog = new Frog(this, frogXStart, frogYStart);
 
-      Log log1 = new Log(0, 180, 'R');
-      obstacles.add(log1);
+  	Log log1 = new Log(0, RIVER_STARTING_Y, 'R');
+  	obstacles.add(log1);
 
-      Log log2 = new Log(480, 80, 'L');
-      obstacles.add(log2);
+  	Log log2 = new Log(480, 166, 'L');
+  	obstacles.add(log2);
 
-      Log log3 = new Log(32,180,'R');
-      obstacles.add(log3);
+  	Log log3 = new Log(32,132,'R');
+  	obstacles.add(log3);
 
-      Log log4 = new Log(480,120,'L');
-      obstacles.add(log4);
+  	Log log4 = new Log(480,98,'L');
+  	obstacles.add(log4);
 
-      Car car1 = new Car(0, 260, 'L');
-      obstacles.add(car1);
+  	Car car1 = new Car(0, 260, 'L');
+  	obstacles.add(car1);
 
-      timer = new Timer(DELAY, this);
-      timer.start();
+  	timer = new Timer(DELAY, this);
+  	timer.start();
   }
 
 
   @Override
   public void paintComponent(Graphics g) {
-      super.paintComponent(g);
-      g.drawImage(background, 0, 0, null);
-      if (running) {
-        doDrawing(g);
-      }
+  	super.paintComponent(g);
+  	g.drawImage(background, 0, 0, null);
 
-      Toolkit.getDefaultToolkit().sync();
+  	if (running) {
+  		doDrawing(g);
+  	}
+
+  	Toolkit.getDefaultToolkit().sync();
   }
 
   private void doDrawing(Graphics g) {
-      Graphics2D g2d = (Graphics2D) g;
+  	Graphics2D g2d = (Graphics2D) g;
 
-      for(GameObstacle o : obstacles) {
-        g2d.drawImage(o.getImage(), o.getX(), o.getY(), this);
-      }
+  	for(GameObstacle o : obstacles) {
+  		g2d.drawImage(o.getImage(), o.getX(), o.getY(), this);
+  	}
 
-      g2d.drawImage(frog.getImage(), frog.getX(), frog.getY(), this);
+  	g2d.drawImage(frog.getImage(), frog.getX(), frog.getY(), this);
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-      updateObstacles();
-      updateFrog();
-      checkCollisions();
-      repaint();
+  	updateObstacles();
+  	updateFrog();
+  	checkCollisions();
+  	repaint();
   }
 
   public void updateFrog() {
-    keyManager.tick();
-    frog.getInput();
-    frog.move();
+  	keyManager.tick();
+  	frog.getInput();
+  	frog.move();
   }
 
   public void updateObstacles() {
-    for(GameObstacle o : obstacles) {
-      o.move();
-    }
+  	for(GameObstacle o : obstacles) {
+  		o.move();
+  	}
   }
 
- public void checkCollisions() {
-    Rectangle player = frog.getBounds();
+  /**
+   * Checks if the player collides with game obstacles.
+   */
+  public void checkCollisions() {
+  	Rectangle player = frog.getBounds();
 
-    if (frog.getY() < RIVER_STARTING_Y) {
-      boolean overLap = false;
-      for (GameObstacle o: obstacles) {
-        Rectangle obs = o.getBounds();
-        if (player.intersects(obs) && !o.isDangerous()) {
-          overLap = true;
-        }
-      }
-      if (!overLap) {
-        frog.die();
-      }
-    }
+  	if (frog.getY() < RIVER_STARTING_Y) {
+  		boolean overLap = false;
+  		for (GameObstacle o: obstacles) {
+  			Rectangle obs = o.getBounds();
+  			if (player.intersects(obs) && !o.isDangerous()) {
+  				overLap = true;
+          frog.setX(o.getX());
+  			}
+  		}
+  		if (!overLap) {
+  			frog.die();
+  		}
+  	}
 
-    for (GameObstacle o : obstacles) {
-      Rectangle obs = o.getBounds();
-      if (player.intersects(obs) && o.isDangerous()) {
-        frog.die();
-      }
-    }
+  	for (GameObstacle o : obstacles) {
+  		Rectangle obs = o.getBounds();
+  		if (player.intersects(obs) && o.isDangerous()) {
+  			frog.die();
+  		}
+  	}
   }
 
+  /**
+   * Getter for keyManager
+   * @return KeyManager the board's KeyManager
+   */
   public KeyManager getKeyManager() {
-    return keyManager;
+  	return keyManager;
   }
 }
