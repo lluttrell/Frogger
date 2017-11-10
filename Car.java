1e03fea459ebaade6616224f7894613cef849e15
Richard Williams <richyw@uvic.ca>
2017-11-10 12:09:13 -0700

    Removed carInit() method as it seemed unneccesary. Added javadoc comments.

--- Car.java
+++ Car.java
@@ -5,22 +5,32 @@
 
 public class Car extends GameObstacle {
 
-    //GUI Constructor
+  /**
+  * GUI constructor for Car Object
+  * @param xStartingLocation Integer representing the x starting location
+  * @param xStartingLocation Integer representing the y starting location
+  * @param direction char representing the direction of movement 'L' for left
+  * 'R' for right.
+  * @param boardWidth Integer representing width of the game area
+  */
     public Car(int xStartingLocation, int yStartingLocation, char direction, int boardWidth) {
         super(xStartingLocation, yStartingLocation, direction, boardWidth);
         setImage("images/asteroid.png");
         getImageDimensions();
-        carInit();
+        setDangerous(true);
     }
 
-    //Text Constructor
+    /**
+    * Text constructor for Car Object
+    * @param xStartingLocation Integer representing the x starting location
+    * @param xStartingLocation Integer representing the y starting location
+    * @param direction char representing the direction of movement 'L' for left
+    * 'R' for right.
+    * @param boardWidth Integer representing width of the game area
+    */
     public Car(int width, int xStartingLocation, int yStartingLocation, char direction, int boardWidth) {
         super(width, xStartingLocation, yStartingLocation, direction, boardWidth);
         setSymbol('X');
-        carInit();
-    }
-
-    private void carInit() {
         setDangerous(true);
     }
 }
