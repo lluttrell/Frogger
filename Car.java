feec049fca965c98ca4b7572d8cc24c9850caf21
Richard Williams <richard.lluttrell@gmail.com>
2017-11-19 09:47:57 -0700

    Added javadoc comments

--- Car.java
+++ Car.java
@@ -5,7 +5,14 @@
 
 public class Car extends GameObstacle {
 
-    //GUI Constructor
+  /**
+  * GUI constructor
+  * @param xStartingLocation Integer representing the x starting location
+  * @param xStartingLocation Integer representing the y starting location
+  * @param direction char representing the direction of movement 'L' for left
+  * 'R' for right.
+  * @param boardWidth Integer representing width of the game area
+  */
     public Car(int xStartingLocation, int yStartingLocation, char direction, int boardWidth) {
         super(xStartingLocation, yStartingLocation, direction, boardWidth);
         setImage("res/asteroid.png");
@@ -13,7 +20,15 @@ public class Car extends GameObstacle {
         carInit();
     }
 
-    //Text Constructor
+    /**
+    * Text based constructor
+    * @param width Integer representing the number of characters wide the car is
+    * @param xStartingLocation Integer representing the x starting location
+    * @param xStartingLocation Integer representing the y starting location
+    * @param direction char representing the direction of movement 'L' for left
+    * 'R' for right.
+    * @param boardWidth Integer representing width of the game area
+    */
     public Car(int width, int xStartingLocation, int yStartingLocation, char direction, int boardWidth) {
         super(width, xStartingLocation, yStartingLocation, direction, boardWidth);
         setSymbol('X');
