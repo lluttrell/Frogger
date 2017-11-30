90dbc159d82c6d11739f67fe4b94a23f35f05d6f
sarah4017 <32577361+sarah4017@users.noreply.github.com>
2017-11-29 19:36:47 -0700

    Add files via upload
    Added extra tests

--- frogger/tests/ModelGUITest.java
+++ frogger/tests/ModelGUITest.java
@@ -1,92 +1,206 @@
-import frogger.*;
-import frogger.util.CountdownTimer;
+//import frogger.Frog;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-
 
 public class ModelGUITest {
-
-    @Test
-    public void test_Constructor() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelGUI modGUI = new ModelGUI(f, obs, countdownTimer);
+    
+	@Test
+    public void test_Constructor(){
+		
+		Frog f = new Frog(4, 5);
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
         assertEquals("Frog position", 4, f.getX(), 0.00001);
         assertEquals("Frog position", 5, f.getY(), 0.00001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogOverlapsWithLog() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Platform(3, 200, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelGUI modGUI = new ModelGUI(f, obs, countdownTimer);
-        modGUI.updateObstacles();
+	} 
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromRight(){
+		
+		Frog f = new Frog(4, 200);
+		f.getImageDimensions();
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,200,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
         f.setX(2);
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromLeft(){
+		
+		Frog f = new Frog(4, 200);
+		f.getImageDimensions();
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,200,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
+        f.setX(5);
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromBelow(){
+		
+		Frog f = new Frog(4, 201);
+		f.getImageDimensions();
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,200,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
         f.setY(200);
         modGUI.checkCollisions();
-
+        
         assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogDoesNotOverlapWithLog() {
-
-        Frog f = new Frog(7, 190);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Platform(3, 200, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelGUI modGUI = new ModelGUI(f, obs, countdownTimer);
-        modGUI.updateObstacles();
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromAbove(){
+		
+		Frog f = new Frog(4, 199);
+		f.getImageDimensions();
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,200,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
+        f.setY(200);
         modGUI.checkCollisions();
-
-        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogDoesNotOverlapWithCar() {
-
-        Frog f = new Frog(4, 250);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Collidable(3, 2, 'L', 240));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelGUI modGUI = new ModelGUI(f, obs, countdownTimer);
-        modGUI.updateObstacles();
-        modGUI.checkCollisions();
-
+        
         assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogOverlapsWithCar() {
-
-        Frog f = new Frog(2, 201);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Collidable(3, 201, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelGUI modGUI = new ModelGUI(f, obs, countdownTimer);
-        modGUI.updateObstacles();
-        f.setX(44);
-        f.setY(201);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogDoesNotOverlapWithLog(){
+		
+		Frog f = new Frog(7, 100);
+		f.getImageDimensions();
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,200,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
         modGUI.checkCollisions();
-
+        
         assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
-    }
-
+	}
+	
+	@Test
+    public void test_checkCollision_FrogDoesNotOverlapWithCar(){
+		
+		Frog f = new Frog(4, 250);
+		f.getImageDimensions();
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,2,'L', 240));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromRight(){
+		
+		Frog f = new Frog(10, 250);
+		f.getImageDimensions();
+		
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,250,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
+        f.setX(3);
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromLeft(){
+		
+		Frog f = new Frog(1, 250);
+		f.getImageDimensions();
+		
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,250,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
+        f.setX(3);
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromAbove(){
+		
+		Frog f = new Frog(3, 249);
+		f.getImageDimensions();
+		
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,250,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles();
+        f.setY(250);
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromBelow(){
+		
+		Frog f = new Frog(2, 251);
+		f.getImageDimensions();
+		
+		ControllerGUI ct = new ControllerGUI();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,250,'L', 15));
+		
+        ModelGUI modGUI = new ModelGUI(f, obs, ct);
+        modGUI.updateObstacles(); 
+        f.setY(250);
+        modGUI.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
 }
--- frogger/tests/ModelTextTest.java
+++ frogger/tests/ModelTextTest.java
@@ -1,120 +1,206 @@
-import frogger.*;
-import frogger.util.CountdownTimer;
+
+import static org.junit.Assert.*;
+
+//import frogger.Frog;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-
 
 public class ModelTextTest {
-
-    @Test
-    public void test_Constructor() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelText modT = new ModelText(f, obs, countdownTimer);
-        assertEquals("Frog position", 4, f.getX(), 0.00001);
-        assertEquals("Frog position", 5, f.getY(), 0.00001);
-    }
-
-
-    @Test
-    public void test_checkCollision_FrogOverlapsWithLog() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Platform(3, 4, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelText modT = new ModelText(f, obs, countdownTimer);
-        modT.updateObstacles();
-        f.setX(2);
-        f.setY(4);
-        modT.checkCollisions();
-
-        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogDoesNotOverlapWithLog() {
-
-        Frog f = new Frog(7, 3);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Platform(3, 5, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelText modT = new ModelText(f, obs, countdownTimer);
-        modT.updateObstacles();
-        modT.checkCollisions();
-
-        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogDoesNotOverlapWithCar() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Collidable(3, 2, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelText modT = new ModelText(f, obs, countdownTimer);
-        modT.updateObstacles();
-        modT.checkCollisions();
-
-        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
-    }
-
-    @Test
-    public void test_checkCollision_FrogOverlapsWithCar() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-
-        obs.add(new Collidable(3, 2, 'L', 15));
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelText modT = new ModelText(f, obs, countdownTimer);
-        modT.updateObstacles();
-        f.setX(2);
-        f.setY(2);
-        modT.checkCollisions();
-
-        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
-    }
-
-	
-	/*@Test
-    public void test_overlapsWith_FrogOverlapsWithCar(){
+    
+	@Test
+    public void test_Constructor(){
 		
 		Frog f = new Frog(4, 5);
+		ControllerText ct = new ControllerText();
 		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
 		
-		Collidable c1 = new Collidable(3,2,'L', 15);
+        ModelText modT = new ModelText(f, obs, ct);
+        assertEquals("Frog position", 4, f.getX(), 0.00001);
+        assertEquals("Frog position", 5, f.getY(), 0.00001);
+	} 
+
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromLeft(){
 		
-        ModelText modT = new ModelText(f, obs);
-        f.setX(3);
-        f.setY(2);
+		Frog f = new Frog(10, 4);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,4,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setX(2);
+        modT.checkCollisions();
         
-        assertEquals("Frog overlaps with car", true, modT.overlapsWith(c1));
-	}*/
-
-    @Test
-    public void test_getRiverStartingY() {
-
-        Frog f = new Frog(4, 5);
-        ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
-        CountdownTimer countdownTimer = new CountdownTimer();
-
-        ModelText modT = new ModelText(f, obs, countdownTimer);
-
-        assertEquals("Y coordinate where river starts should be 4", 4, modT.getRiverStartingY(), 0.000001);
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromRight(){
+		
+		Frog f = new Frog(10, 4);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,4,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setX(4);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromBelow(){
+		
+		Frog f = new Frog(4, 5);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,4,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setY(4);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithLogFromAbove(){
+		
+		Frog f = new Frog(4, 3);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,4,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setY(4);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogDoesNotOverlapWithLog(){
+		
+		Frog f = new Frog(7, 3);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Log(3,5,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogDoesNotOverlapWithCar(){
+		
+		Frog f = new Frog(4, 5);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,2,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should still be 3", 3, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromRight(){
+		
+		Frog f = new Frog(10, 2);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,2,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setX(2);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromLeft(){
+		
+		Frog f = new Frog(10, 2);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,2,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setX(4);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromAbove(){
+		
+		Frog f = new Frog(3, 1);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,2,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setY(2);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+    public void test_checkCollision_FrogOverlapsWithCarFromBelow(){
+		
+		Frog f = new Frog(3,3);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+		
+		obs.add(new Car(3,2,'L', 15));
+		
+        ModelText modT = new ModelText(f, obs, ct);
+        modT.updateObstacles(); 
+        f.setY(2);
+        modT.checkCollisions();
+        
+        assertEquals("Frog's lives should be 2", 2, f.getLives(), 0.000001);
+	}
+	
+	@Test
+	public void test_getRiverStartingY() {
+		
+		Frog f = new Frog(4, 5);
+		ControllerText ct = new ControllerText();
+		ArrayList<GameObstacle> obs = new ArrayList<GameObstacle>();
+	    
+	    ModelText modT = new ModelText(f, obs, ct);
+	    
+	    assertEquals("Y coordinate where river starts should be 4", 4, modT.getRiverStartingY(), 0.000001);
     }
+	
 }
