e0164bfbd15f137853b5a61a2d8d27ddc3b9ff38
idenc <iden.craven@gmail.com>
2017-12-04 23:53:01 -0700

    Added reset button "X"

--- frogger/model/Model.java
+++ frogger/model/Model.java
@@ -18,6 +18,9 @@ public class Model {
 
     /**
      * Constructor for Model
+     * frog, obstacles, and countdownTimer are all privacy leaked.
+     * frog is privacy leaked due to its values being constantly updated and View needing direct access to those values.
+     * obstacles is also privacy leaked due to needing access to values being updated constantly.
      *
      * @param frog      The Frog object which the player controls.
      * @param obstacles An ArrayList containing the Game Obstacles.
@@ -78,5 +81,9 @@ public class Model {
 
     public int getScore() {
         return score;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
     }
 }
